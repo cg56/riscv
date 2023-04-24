b664685d0ce8c6e24bfdbf4610a51d0e48d89f79
Colm Gavin <colm@colmgavin.com>
2023-04-23 21:36:13 -0700

    First version

---
+++ bootrom/bin2rom.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int main(int argc, const char *argv[])
+{
+    FILE *fp = fopen(argv[1], "r");
+    if (fp == 0)
+        printf("Error");
+
+    unsigned int word;
+    int addr = 0;
+
+    while (fread(&word, 1, 4, fp) == 4)
+        printf("\t\t\t\t%d:\tword <= 32'h%08x;\n", addr++, word);
+
+    fclose(fp);
+}
+
